9d00dc3f320082a0f75221137225e9f3ed99bc10
babyshoes <dianecai@gmail.com>
2015-07-31 16:19:42 -0400

    all done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array)
+  i = 0
+  while i < array.length
+  	yield(array[i])
+  	i += 1
+  end
+  array
+end
+
+tas = ['arel', 'spencer', 'jon', 'logan']
+
+my_each(tas) do |ta|
+  puts "#{ta} is awesome"
 end
